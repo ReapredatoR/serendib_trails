fb991ccfa7006a10eb326796ae1371f88d7833a1
reapredator <0606malingarajapaksha@gmail.com>
2024-05-23 11:22:01 +0530

    community screen

--- lib/screens/community_screen.dart
+++ lib/screens/community_screen.dart
@@ -1,10 +1,113 @@
 import 'package:flutter/material.dart';
 
-class CommunityScreen extends StatelessWidget {
+class CommunityScreen extends StatefulWidget {
+  @override
+  _CommunityScreenState createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+  int _selectedIndex = 0;
+
+  final List<Map<String, dynamic>> _posts = [
+    {
+      'image': 'lib/images/1.jpg',
+      'title': 'Amazing views from Chariot Path!',
+      'likes': 120,
+      'comments': 45,
+    },
+    {
+      'image': 'lib/images/QVZkVjV3b1FkWFNpU09WRg.jpeg',
+      'title': 'Exploring the Dolukanda (දොලුකන්ද)',
+      'likes': 80,
+      'comments': 30,
+    },
+    {
+      'image': 'lib/images/QVZkVjV3b1FkWFNpU09WRg.jpeg',
+      'title': 'Exploring the Knuckles Mountain Range',
+      'likes': 200,
+      'comments': 70,
+    },
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildPost(Map<String, dynamic> post) {
+    return Card(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.asset(
+            post['image'],
+            fit: BoxFit.cover,
+            height: 200, // Fixed height for all images
+            width: double.infinity,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              post['title'],
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.thumb_up),
+                onPressed: () {},
+              ),
+              Text('${post['likes']} likes'),
+              IconButton(
+                icon: Icon(Icons.comment),
+                onPressed: () {},
+              ),
+              Text('${post['comments']} comments'),
+              IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Community Screen - Here will be the community features'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Community'),
+          bottom: TabBar(
+            onTap: _onItemTapped,
+            tabs: <Widget>[
+              Tab(text: 'Posts'),
+              Tab(text: 'Chat with Hikers'),
+              Tab(text: 'Make a Difference'),
+            ],
+          ),
+        ),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: <Widget>[
+            ListView.builder(
+              itemCount: _posts.length,
+              itemBuilder: (context, index) {
+                return _buildPost(_posts[index]);
+              },
+            ),
+            Center(child: Text('Chat with other hikers - Coming Soon')),
+            Center(child: Text('Think and Make a Difference - Coming Soon')),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/trail_list_screen.dart
+++ lib/screens/trail_list_screen.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
 
+class Trail {
+  final String name;
+  final double miles;
+  final double time; // Time in hours
+  final double stars; // Rating out of 5
+
+  Trail(
+      {required this.name,
+      required this.miles,
+      required this.time,
+      required this.stars});
+}
+
 class TrailListScreen extends StatelessWidget {
-  final List<String> trails = [
-    "Adam's Peak (Sri Pada)",
-    "Ella Rock",
-    "Little Adam's Peak",
-    "Knuckles Mountain Range",
-    "Horton Plains National Park",
-    "Sinharaja Rainforest",
-    "Pidurangala Rock"
+  final List<Trail> trails = [
+    Trail(name: "Adam's Peak (Sri Pada)", miles: 5.5, time: 3.0, stars: 4.8),
+    Trail(name: "Ella Rock", miles: 3.1, time: 2.5, stars: 4.5),
+    Trail(name: "Little Adam's Peak", miles: 1.8, time: 1.0, stars: 4.2),
+    Trail(name: "Knuckles Mountain Range", miles: 7.3, time: 4.0, stars: 4.7),
+    Trail(
+        name: "Horton Plains National Park", miles: 6.2, time: 3.5, stars: 4.9),
+    Trail(name: "Sinharaja Rainforest", miles: 4.6, time: 2.8, stars: 4.6),
+    Trail(name: "Pidurangala Rock", miles: 2.2, time: 1.5, stars: 4.3)
   ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: trails.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(trails[index]),
-          onTap: () {
-            // Navigate to trail details screen
-          },
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trail List'),
+      ),
+      body: ListView.builder(
+        itemCount: trails.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(trails[index].name),
+            subtitle: Row(
+              children: [
+                Icon(Icons.star, size: 16.0, color: Colors.amber),
+                SizedBox(width: 4.0),
+                Text('${trails[index].stars}'),
+                SizedBox(width: 16.0),
+                Icon(Icons.directions_walk, size: 16.0, color: Colors.blue),
+                SizedBox(width: 4.0),
+                Text('${trails[index].miles} miles'),
+                SizedBox(width: 16.0),
+                Icon(Icons.timer, size: 16.0, color: Colors.green),
+                SizedBox(width: 4.0),
+                Text('${trails[index].time} hours'),
+              ],
+            ),
+            onTap: () {
+              // Navigate to trail details screen
+            },
+          );
+        },
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: TrailListScreen(),
+  ));
 }
